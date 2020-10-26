472ebbef16bd7b8934090fb7166e93d68554d593
Mattias Juhl <mattias.juhl@gmail.com>
2020-10-27 10:33:13 +1100

    test for JSON

--- .github/workflows/publishchanges.py
+++ .github/workflows/publishchanges.py
@@ -130,7 +130,7 @@ for modification in modifications:
     defect = add_data(modification[1])
 
     updateRequest = requests.put(basePath + "/" + modification[1],
-                                  data=json.dumps(defect.__dict__),
+                                  data=defect.JSONData,
                                   headers=headers)
     handleHttpResponse(updateRequest, "Could not update " + modification[1])
 
